b659ce7a316c1f33bfe4d25915c140db43727bda
unknown <saniel.alves@grupomateus.com.br>
2026-08-30 22:51:35 -0300

    fix: usa wildcard no CORS para aceitar todas as origens (temporário)

--- backend/main.py
+++ backend/main.py
@@ -8,12 +8,8 @@ app = FastAPI(title=settings.app_name)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "https://aprendizado-railway-vercel-7skqq3yyi-sanielbarros-projects.vercel.app",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
